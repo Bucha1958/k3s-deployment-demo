9b32c593c3d098e3a6581f424a794138f4c4d3ee
Bucha1958 <okoriemmadu@gmail.com>
2025-05-12 07:55:20 +0100

    added today

--- general-service/src/app.ts
+++ general-service/src/app.ts
@@ -32,14 +32,6 @@ app.use('/api', uploadRoutes);
 app.post('/api/logout', (req: Request, res: Response) => {
     res.cookie('token', '').json('ok');
 })
-
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'dist')));
-
-// // The catch-all handler: for any request that doesn't match one above, send back index.html
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
   
 connectToDatabase()
 
