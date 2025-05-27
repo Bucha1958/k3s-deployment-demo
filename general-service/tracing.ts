6887ac38512c2d2bddc7a0c4a438cc1c2534452d
Bucha1958 <okoriemmadu@gmail.com>
2025-05-27 10:02:24 +0100

    collector agent configuration

--- general-service/tracing.ts
+++ general-service/tracing.ts
@@ -2,13 +2,16 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import {
   PeriodicExportingMetricReader,
   ConsoleMetricExporter,
 } from '@opentelemetry/sdk-metrics';
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces', // Collector OTLP HTTP endpoint
+  }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
   }),
